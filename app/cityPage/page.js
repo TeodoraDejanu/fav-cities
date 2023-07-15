9ea47f703b6930428489dfc85145cb380947ef14
TeodoraDejanu <TeodoraDejanu@users.noreply.github.com>
2023-07-15 20:19:45 +0300

    Create new pages and navbar component

--- app/cityPage/page.js
+++ app/cityPage/page.js
@@ -1,11 +1,11 @@
-import Nav from "./components/nav";
+import Nav from "../components/nav";
 
-export default function Home() {
+export default function CityPage() {
   return (
     <div>
       <Nav />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-4xl font-bold">Home</h1>
+        <h1 className="text-4xl font-bold">City page</h1>
       </main>
     </div>
   );
---
+++ app/components/nav.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function Nav() {
+  return (
+    <nav className="flex py-4 px-6 border-b border-gray-200">
+      <Link href="/">Home</Link>
+      <Link href="/search" className="ml-2">
+        Search
+      </Link>
+      <Link href="/cityPage" className="ml-2">
+        City page
+      </Link>
+      <Link href="/favourites" className="ml-2">
+        Favourites
+      </Link>
+    </nav>
+  );
+}
