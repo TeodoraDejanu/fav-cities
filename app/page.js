ef09170e298205cf59bdc6ca3c67d96fb46c05db
TeodoraDejanu <TeodoraDejanu@users.noreply.github.com>
2023-07-19 23:55:11 +0300

    Add random cities in Home page

--- app/page.js
+++ app/page.js
@@ -10,6 +10,11 @@ export default function Home() {
         <div className="mb-8">
           <SearchButton />
         </div>
+        <div className="mb-8">
+          <h5>You can search for information about cities such as:</h5>
+          <h3>Constanta, Arad, Dubai, New York, Bakersfield</h3>
+          <h5>and more</h5>
+        </div>
       </main>
     </div>
   );
